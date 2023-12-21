195ee8c04d267aea12f19b64bc0aa1cebf1437ef
polendina <solicitmind@gmail.com>
2023-12-21 05:14:43 +0200

    test: use junit4 instead of junit5 and coroutine testing - Getting Coroutine testing working for the viewmodel - Employing Junit4 instead of Junit5 project-wide

--- app/src/test/kotlin/com/polendina/knounce/presentation/shared/floatingbubble/FloatingBubbleViewModelImplTest.kt
+++ app/src/test/kotlin/com/polendina/knounce/presentation/shared/floatingbubble/FloatingBubbleViewModelImplTest.kt
@@ -3,20 +3,23 @@ package com.polendina.knounce.presentation.shared.floatingbubble
 import android.app.Application
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
-import com.polendina.knounce.data.database.DatabaseMock
+import com.polendina.knounce.data.database.DatabaseImpl
 import com.polendina.knounce.data.database.Word
+import com.polendina.knounce.utils.refine
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import org.junit.Assert.assertEquals
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import kotlin.time.Duration
 
 //@RunWith(MockitoJUnitRunner::class)
 @RunWith(RobolectricTestRunner::class)
@@ -27,39 +30,39 @@ class FloatingBubbleViewModelImplTest {
     private var germanWords = listOf(
         Word(
             title = "nacht",
-            translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
-            pronunciations = null,
-            false
+            translation = mutableStateMapOf("noun" to mutableStateListOf(Word.Translation("night", null))),
+            pronunciations = mutableListOf("nacht" to "https://audio00.forvo.com/mp3/9218640/34/9218640_34_5784.mp3", "Verklärte Nacht" to "https://audio00.forvo.com/mp3/9170558/34/9170558_34_1767068_1.mp3", "Nacht-und-Nebel-Erlass" to "https://audio00.forvo.com/mp3/9218640/34/9218640_34_1906941.mp3", "Nacht-und-Nebel-Aktion" to "https://audio00.forvo.com/mp3/9223905/34/9223905_34_1906943_1.mp3", "Tag-Nacht-Rhythmus" to "https://audio00.forvo.com/mp3/9233940/34/9233940_34_3121228.mp3", "Frühjahrs-Tag-und-Nacht-Gleiche" to "https://audio00.forvo.com/mp3/9218640/34/9218640_34_3199878.mp3", "Tag-und-Nacht-Gleiche" to "https://audio00.forvo.com/mp3/9218640/34/9218640_34_3199881.mp3", "Tag-Nacht-Amplitude" to "https://audio00.forvo.com/mp3/9787104/34/9787104_34_5597595.mp3", "Über-Nacht-Lieferung" to "https://audio00.forvo.com/mp3/8973927/34/8973927_34_6069024.mp3"),
+            loaded = false
         ),
         Word(
             title = "milch",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
             pronunciations = null,
-            false
+            loaded = false
         ),
         Word(
             title = "ich",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
             pronunciations = null,
-            false
+            loaded = false
         ),
         Word(
             title = "ich",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
             pronunciations = null,
-            false
+            loaded = false
         ),
         Word(
             title = "",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
             pronunciations = null,
-            false
+            loaded = false
         ),
         Word(
             title = " ",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
             pronunciations = null,
-            false
+            loaded = false
         ),
         // Following 2 are empty
         Word(
@@ -67,79 +70,70 @@ class FloatingBubbleViewModelImplTest {
     //            translation = "spec",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
             pronunciations = null,
-            false
+            loaded = false
         ),
         Word(
             title = "@$@!#$",
     //            translation = "@$@!#$",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
             pronunciations = null,
-            false
+            loaded = false
         ),
         Word(
             title = "Mediterrane Pflanzen halten das locker aus.\n Aber auch die brauchen ab und an mal Wasser",
             translation = mutableStateMapOf("" to mutableStateListOf(Word.Translation(null, null))),
     //            translation = "Mediterranean plants can easily handle this. But they also need water from time to time",
             pronunciations = null,
-            false
+            loaded = false
         )
     )
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Before
-    fun setup() {
-        val testDispatcher = UnconfinedTestDispatcher()
-        Dispatchers.setMain(testDispatcher)
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
 //        application = Mockito.mock(Application::class.java)
         application = RuntimeEnvironment.getApplication()
-        floatingBubbleViewModelImpl = FloatingBubbleViewModelImpl(application, testDispatcher, database = DatabaseMock())
+        floatingBubbleViewModelImpl = FloatingBubbleViewModelImpl(
+            application = application,
+            database = DatabaseImpl(application = application)
+        )
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
-    fun translateWord() = runTest {
-//        floatingBubbleViewModel.words.clear()
-//        germanWords.subList(0, 1).forEach {
-//            floatingBubbleViewModel.words.add(Word(title = it.title.refine()))
-//            floatingBubbleViewModel.currentWord = floatingBubbleViewModel.words.last()
-//            floatingBubbleViewModel.translateWord().join()
-//        }
-        floatingBubbleViewModelImpl.currentWord = Word("danke", null, null, false)
-        floatingBubbleViewModelImpl.translateWord().join()
-        println(floatingBubbleViewModelImpl.currentWord)
-//        assertEquals(
-//            germanWords.map { it.translation },
-//            val j = floatingBubbleViewModel.words.map { it.translation }
-//            println(j)
-//        )
-    }
-
-    @Test
-    fun grabAudioFiles() = runTest {
-        germanWords.map { it.title }.map {
-            floatingBubbleViewModelImpl.grabAudioFiles(
-                searchTerm = it
-            ).run {
-                println(this?.attributes?.total)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun translateWordTest() = runTest {
+        germanWords.take(1).forEach {
+            Word(title = it.title.refine()).let {
+                floatingBubbleViewModelImpl.words.add(it)
+                floatingBubbleViewModelImpl.currentWord = it
             }
-        }.let {
-            println(it)
-//            assertEquals(
-//                germanWords.map { it.pronunciations.isNotEmpty() },
-//                it
-//            )
+            floatingBubbleViewModelImpl.translateWord().join()
         }
+        advanceUntilIdle()
+        assert(
+            germanWords.take(1).map { it.translation?.values }.map { it?.toList()?.map { it.toList() } } ==
+            floatingBubbleViewModelImpl.words.map { it.translation?.values?.toList()?.map { it.toList() } }
+        )
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun loadPronunciations() = runTest {
-        germanWords.subList(0, 1).forEach {
-            println(it.title)
-//            floatingBubbleViewModel.srcWord = TextFieldValue(it.title)
-            floatingBubbleViewModelImpl.currentWord = it
-            floatingBubbleViewModelImpl.loadPronunciations(word = it).join()
+    fun loadPronunciations() = runTest(timeout = Duration.INFINITE) {
+        germanWords.take(1).forEachIndexed { index, word ->
+            floatingBubbleViewModelImpl.searchWord(word.title)
+            floatingBubbleViewModelImpl.loadPronunciations()
         }
-        println(floatingBubbleViewModelImpl.currentWord.pronunciations?.map { it.first })
-//        println(floatingBubbleViewModel.words.toList())
+        advanceUntilIdle()
+        assert(
+            floatingBubbleViewModelImpl.words.map { it.pronunciations?.take(3)?.toList() } ==
+            germanWords.take(1).map { it.pronunciations?.take(3)?.toList() }
+        )
     }
 
     @Test
@@ -147,8 +141,8 @@ class FloatingBubbleViewModelImplTest {
         germanWords.map { it.title }.forEach {
             floatingBubbleViewModelImpl.searchWord(it)
         }
-        assertEquals(
-            germanWords.map { it.title }.filter { it.isNotBlank() }.distinct(),
+        assert(
+            germanWords.map { it.title }.filter { it.isNotBlank() }.distinct() ==
             floatingBubbleViewModelImpl.words.toList().map { it.title }
         )
     }
